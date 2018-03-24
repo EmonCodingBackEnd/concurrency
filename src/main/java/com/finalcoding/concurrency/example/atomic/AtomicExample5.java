433edf2bba5063d254dad2d804cde940dd5562c7
Rushing <liming20110711@163.com>
2018-03-24 09:52:06 +0800

    atomic and synchronized

---
+++ src/main/java/com/finalcoding/concurrency/example/atomic/AtomicExample5.java
@@ -0,0 +1,53 @@
+/*
+ * 文件名称：ConcurrencyTest.java
+ * 系统名称：[系统名称]
+ * 模块名称：[模块名称]
+ * 软件版权：Copyright (c) 2011-2018, liming20110711@163.com All Rights Reserved.
+ * 功能说明：[请在此处输入功能说明]
+ * 开发人员：Rushing0711
+ * 创建日期：20180324 07:52
+ * 修改记录：
+ * <Version>        <DateSerial>        <Author>        <Description>
+ * 1.0.0            20180324-01         Rushing0711     M201803240752 新建文件
+ ********************************************************************************/
+package com.finalcoding.concurrency.example.atomic;
+
+import com.finalcoding.concurrency.annotations.ThreadSafe;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * [请在此输入功能简述].
+ *
+ * <p>创建时间: <font style="color:#00FFFF">20180324 07:52</font><br>
+ * [请在此输入功能详述]
+ *
+ * @author Rushing0711
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Slf4j
+@ThreadSafe
+public class AtomicExample5 {
+
+    // 必须volatile修饰，且非static修饰
+    @Getter public volatile int count = 100;
+
+    private static AtomicIntegerFieldUpdater<AtomicExample5> updater =
+            AtomicIntegerFieldUpdater.newUpdater(AtomicExample5.class, "count");
+
+    public static void main(String[] args) {
+        AtomicExample5 example5 = new AtomicExample5();
+        if (updater.compareAndSet(example5, 100, 120)) {
+            log.info("update success 1, {}", example5.getCount());
+        }
+        if (updater.compareAndSet(example5, 100, 120)) {
+            log.info("update success 2, {}", example5.getCount());
+        } else {
+            log.info("update failed, {}", example5.getCount());
+        }
+    }
+}
---
+++ src/main/java/com/finalcoding/concurrency/example/sync/SynchronizedExample2.java
@@ -0,0 +1,62 @@
+/*
+ * 文件名称：SynchronizedExample1.java
+ * 系统名称：[系统名称]
+ * 模块名称：[模块名称]
+ * 软件版权：Copyright (c) 2011-2018, liming20110711@163.com All Rights Reserved.
+ * 功能说明：[请在此处输入功能说明]
+ * 开发人员：Rushing0711
+ * 创建日期：20180324 09:22
+ * 修改记录：
+ * <Version>        <DateSerial>        <Author>        <Description>
+ * 1.0.0            20180324-01         Rushing0711     M201803240922 新建文件
+ ********************************************************************************/
+package com.finalcoding.concurrency.example.sync;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * [请在此输入功能简述].
+ *
+ * <p>创建时间: <font style="color:#00FFFF">20180324 09:22</font><br>
+ * [请在此输入功能详述]
+ *
+ * @author Rushing0711
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Slf4j
+public class SynchronizedExample2 {
+
+    // 修饰一个类
+    public static void test1(int j) {
+        synchronized (SynchronizedExample2.class) {
+            for (int i = 0; i < 10; i++) {
+                log.info("test1 {} - {}", j, i);
+            }
+        }
+    }
+
+    // 修饰一个静态方法
+    public static synchronized void test2(int j) {
+        for (int i = 0; i < 10; i++) {
+            log.info("test2 {} - {}", j, i);
+        }
+    }
+
+    public static void main(String[] args) {
+        SynchronizedExample2 example1 = new SynchronizedExample2();
+        SynchronizedExample2 example2 = new SynchronizedExample2();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(
+                () -> {
+                    example1.test1(1);
+                });
+        executorService.execute(
+                () -> {
+                    example2.test1(2);
+                });
+    }
+}
